54899e3c16dec66015155ddd0652da0ab0fcc6c9
Fernando Pacheco <131721500+fernando-pacheco@users.noreply.github.com>
2023-12-26 14:47:41 -0300

    adicionando views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 
+def login(request):
+    return render(request, 'users/login.html')
+
+
+def cadastro(request):
+    return render(request, 'users/cadastro.html')
+
